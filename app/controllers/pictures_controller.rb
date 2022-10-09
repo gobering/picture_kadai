ef6faba4f6c08f531cdea4dc7711cedc8b948544
sho ebisui <raphi.jus1412@gmail.com>
2022-10-09 20:41:11 +0900

    crud機能作った

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,66 @@
+class PicturesController < ApplicationController
+  before_action :set_picture, only: %i[ show edit update destroy ]
+
+  def index
+    @pictures = Picture.all
+  end
+
+  def show
+  end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def edit
+  end
+
+  def create
+    @picture = current_user.pictures.build(picture_params)
+
+    if params[:back]
+      render :new
+    else
+      if @picture.save
+        redirect_to pictures_path, notice: "投稿しました！"
+      else
+        render :new
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @picture.update(picture_params)
+        format.html { redirect_to picture_url(@picture), notice: "更新しました!" }
+        format.json { render :show, status: :ok, location: @picture }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @picture.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @picture.destroy
+
+    respond_to do |format|
+      format.html { redirect_to pictures_url, notice: "投稿を削除しました!" }
+      format.json { head :no_content }
+    end
+  end
+
+  def confirm
+    @picture = current_user.pictures.build(picture_params)
+    render :new if @picture.invalid?
+  end
+
+  private
+    def set_picture
+      @picture = Picture.find(params[:id])
+    end
+
+    def picture_params
+      params.require(:picture).permit(:image, :content, :user_id, :image_cache)
+    end
+end
