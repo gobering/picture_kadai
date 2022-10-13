3931bf4869328bfa83dcdaa6cabc14b6f819b20e
sho ebisui <raphi.jus1412@gmail.com>
2022-10-13 17:47:43 +0900

    改行直し

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,6 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
       if @user.save
         redirect_to user_path(@user.id)
       else
@@ -40,7 +39,6 @@ class UsersController < ApplicationController
 
   def destroy
     @user.destroy
-
     respond_to do |format|
       format.html { redirect_to users_url, notice: "User was successfully destroyed." }
       format.json { head :no_content }
