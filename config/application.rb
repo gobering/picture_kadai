122952c74d2ef94f2c32338ff2fbcce04853e599
sho ebisui <raphi.jus1412@gmail.com>
2022-10-27 23:18:38 +0900

    アプリケーションの国際化をした

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,12 @@ module ManApp
                        controller_specs: false,
                        request_specs: false
     end
+    
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
   end
 end
