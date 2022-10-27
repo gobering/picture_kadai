d16ed2334827967bcf6d4a3b41a1dd3d4afb78b4
sho ebisui <raphi.jus1412@gmail.com>
2022-10-27 17:25:07 +0900

    Model specが完成した

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -8,14 +8,16 @@ RSpec.describe Task, type: :model do
         expect(task).not_to be_valid
       end
     end
-       context 'タスクの詳細が空の場合' do
+    context 'タスクの詳細が空の場合' do
       it 'バリデーションにひっかかる' do
-        # ここに内容を記載する
+        task1 = Task.new(title: '', content: '')
+        expect(task1).not_to be_valid
       end
     end
     context 'タスクのタイトルと詳細に内容が記載されている場合' do
       it 'バリデーションが通る' do
-        # ここに内容を記載する
+        task2 = Task.new(title: 'aiueo', content: 'aiueo')
+        expect(task2).to be_valid
       end
     end
   end
