ed64e221b5c806bdc6e4acfeb932456754ebc94b
sho ebisui <raphi.jus1412@gmail.com>
2022-10-31 22:28:20 +0900

    emailにユニーク制約をつけた

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true
-  validates :email, presence: true, length:{maximum:50}
-              #  format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :email, uniqueness: true, presence: true, length:{maximum:50}
   before_validation { email.downcase! }
   validates :password, presence: true
 end
