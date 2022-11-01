23552744f4a606798193bbbb5374d38ecb5bdac3
sho ebisui <raphi.jus1412@gmail.com>
2022-11-01 22:30:13 +0900

    管理画面のそれぞれのview画面を作った

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,6 @@
 class Admin::UsersController < ApplicationController
+  before_action :set_user, only: %i[ show edit update destroy ]
+
   def index
     @users = User.all
   end
@@ -10,7 +12,7 @@ class Admin::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to admin_users_path(@user)
+      redirect_to admin_users_path(@user), notice: "ユーザー情報を登録しました"
     else
       render :new
     end
@@ -22,10 +24,25 @@ class Admin::UsersController < ApplicationController
   def edit
   end
 
+  def update
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), notice: "ユーザー情報を更新しました"
+    else
+      redirect_to edit_admin_user_path(@user)
+    end
+  end
+
   def destroy
+    @user.destroy
+    redirect_to admin_users_path, notice: "ユーザー情報を消去しました"
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+    
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
