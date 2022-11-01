de69690fcc8886da9c8d1f4535c19844952e3610
sho ebisui <raphi.jus1412@gmail.com>
2022-11-01 15:42:09 +0900

    userテーブルにadminカラムを追加した

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root "tasks#index"
   get "/" => "tasks#index"
   resources :tasks 
-  resources :users, only: [:new, :create, :show]
+  namespace :admin do
+    resources :users
+  end
   resources :sessions, only: [:new, :create, :destroy]
 end
