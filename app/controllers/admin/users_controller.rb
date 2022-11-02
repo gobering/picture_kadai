1cd12bca292d20b40db23cf1977493974303b78c
sho ebisui <raphi.jus1412@gmail.com>
2022-11-02 11:12:49 +0900

    ユーザを削除したら、そのユーザに紐づいているタスクを全て削除するようにした

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,7 +2,7 @@ class Admin::UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
 
   def index
-    @users = User.all.includes(:task)
+    @users = User.all.includes(:tasks)
   end
 
   def new 
